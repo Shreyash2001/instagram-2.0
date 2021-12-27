51495fd6349207d43273924ff9deb324454ac79e
Shreyash <shreyashkatare21@gmail.com>
2021-12-27 23:47:31 +0530

    Worked on login page

---
+++ frontend/src/Screens/Login.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Login.css'
+import Button from '@mui/material/Button';
+
+function Login() {
+    return (
+        <div className="login">
+           <div className="login__left">
+            <div>
+                <img className="login__leftImage1" src="https://res.cloudinary.com/cqn/image/upload/v1640541759/vdbkoefdprxtsiswhjqc_ywbosn.jpg" alt="reels" />
+            </div>
+            <div style={{position:"absolute", top:"80px", left:"300px"}}>
+                <img className="login__leftImage2" src="https://res.cloudinary.com/cqn/image/upload/v1640612515/611ff39fcd52e2797ef79c8f_ig-reels-dancing_v2_fbwcnv.jpg" alt="reels" />
+            </div>
+            <div style={{position:"absolute", top:"400px", left:"0px"}}>
+                <img className="login__leftImage3" src="https://res.cloudinary.com/cqn/image/upload/v1640612573/msid-78574553_width-1200_height-900_resizemode-4_atspem.jpg" alt="reels" />
+            </div>
+            <div style={{position:"absolute", top:"400px", left:"300px"}}>
+                <img className="login__leftImage4" src="https://res.cloudinary.com/cqn/image/upload/v1640612624/add-links-instagram-posts_ccer7p.jpg" alt="reels" />
+            </div>
+
+
+           </div>
+
+           <div className="login__right">
+                <div className="login__rightContainer">
+                    <div>
+                        <h1>Login to connect the world.</h1>
+                    </div>
+
+                    <div className="login__rightContainerUserName">
+                        <div>
+                            <input type="text" placeholder="Enter email or username" />
+                        </div>
+                    </div>
+
+                    <div className="login__rightContainerName">
+                        <div>
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <div>
+                            <input type="password" placeholder="Confirm Password" />
+                        </div>
+                    </div>
+                    
+                    <div className="login__rightContainerButton">
+                        <Button>Login</Button>
+                    </div>
+
+                </div>
+           </div>
+        </div>
+    )
+}
+
+export default Login
--- frontend/src/Screens/Registration.js
+++ frontend/src/Screens/Registration.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import './Registration.css'
 import Button from '@mui/material/Button';
+
 function Registration() {
     return (
         <div className="registration">
            <div className="registration__left">
             <div>
-                <img src="https://res.cloudinary.com/cqn/image/upload/v1640541759/vdbkoefdprxtsiswhjqc_ywbosn.jpg" alt="reels" />
+                <img className="registration__leftImage1" src="https://res.cloudinary.com/cqn/image/upload/v1640541759/vdbkoefdprxtsiswhjqc_ywbosn.jpg" alt="reels" />
             </div>
+            <div style={{position:"absolute", top:"80px", left:"300px"}}>
+                <img className="registration__leftImage2" src="https://res.cloudinary.com/cqn/image/upload/v1640612515/611ff39fcd52e2797ef79c8f_ig-reels-dancing_v2_fbwcnv.jpg" alt="reels" />
+            </div>
+            <div style={{position:"absolute", top:"400px", left:"0px"}}>
+                <img className="registration__leftImage3" src="https://res.cloudinary.com/cqn/image/upload/v1640612573/msid-78574553_width-1200_height-900_resizemode-4_atspem.jpg" alt="reels" />
+            </div>
+            <div style={{position:"absolute", top:"400px", left:"300px"}}>
+                <img className="registration__leftImage4" src="https://res.cloudinary.com/cqn/image/upload/v1640612624/add-links-instagram-posts_ccer7p.jpg" alt="reels" />
+            </div>
+
+
            </div>
 
            <div className="registration__right">
