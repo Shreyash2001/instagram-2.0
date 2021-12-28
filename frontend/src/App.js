c0e6515135c7c37b2dc2108d300672a7ff18fbcf
Shreyash <shreyashkatare21@gmail.com>
2021-12-28 23:41:04 +0530

    Added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Registration from './Screens/Registration';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from './Screens/Login';
+
 
 function App() {
   return (
-    <div className="App">
-      <Registration />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Registration />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
