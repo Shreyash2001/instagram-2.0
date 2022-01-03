0723e4e8f3b50a92c9c98d646cda1fb17deb72c0
Shreyash <shreyashkatare21@gmail.com>
2022-01-03 19:24:50 +0530

    Worked on preferences frontend

--- frontend/src/Components/PreferencesCard.js
+++ frontend/src/Components/PreferencesCard.js
@@ -1,21 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./PreferencesCard.css";
+import Avatar from '@mui/material/Avatar';
 
-function PreferencesCard({name, image, id, show}) {
+function PreferencesCard({name, image, id}) {
+    const [show, setShow] = useState(false);
+    
     return (
-        <div className="preferencesCard">
-            <div>
-                <img src={image} alt={name} />
-                <span>21K Followers</span>
-                <h1>{name}</h1>
-                <div />
-            </div>
-            {show && 
-                <div className="preferencesCard__tick">
+        <>
+        {show ?  <div onClick={() => setShow(!show)} className="preferencesCard">
+            <div className="preferencesCard__container">
+            <div className="preferencesCard__tick">
                     <img className="preferencesCard__tickImage" src="https://res.cloudinary.com/cqn/image/upload/v1641142317/Daco_4675609_di9inc.png" alt="tick" />
                 </div>
-            }
+                <img className="preferencesCard__containerImage" src={image} alt={name} />
+                
+                <div className="preferencesCard__containerInfo">
+                <div>
+                    <Avatar />
+                </div>
+                <div>
+                    <span className="preferencesCard__containerInfoFollowers">21K Followers</span>
+                    <h1 className="preferencesCard__containerInfoName">{name}</h1>
+                </div>
+                </div>
+
+                <div className="preferencesCard__containerBlurTrue" />
+            </div>
         </div>
+        :
+        <div onClick={() => setShow(!show)} className="preferencesCard">
+            <div className="preferencesCard__container">
+                <img className="preferencesCard__containerImage" src={image} alt={name} />
+
+                
+                <div className="preferencesCard__containerInfo">
+                <div>
+                    <Avatar />
+                </div>
+                <div>
+                    <span className="preferencesCard__containerInfoFollowers">21K Followers</span>
+                    <h1 className="preferencesCard__containerInfoName">{name}</h1>
+                </div>
+                </div>
+
+                <div className="preferencesCard__containerBlur" />
+            </div>
+        </div>
+        }
+        </>
     )
 }
 
--- frontend/src/Screens/Preferences.js
+++ frontend/src/Screens/Preferences.js
@@ -8,35 +8,34 @@ function Preferences() {
             <div className="preferences__container">
                 <div className="preferences__containerTop">
                     <h1>Help us set up feed for you</h1>
-                    <p>Follow few of them to make you get going</p>
+                    <span>Click to follow them to make you get going</span>
                 </div>
 
                 <div className="preferences__containerCards">
                     <PreferencesCard 
                         name="Test Tester"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
-                        show = {false}
+                        image="https://www.denofgeek.com/wp-content/uploads/2020/01/peaky_blinders_tommy_cap.jpg?resize=768%2C432"
                     />
                     <PreferencesCard 
                         name="Test Testing"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
+                        image="https://cdn.images.express.co.uk/img/dynamic/20/590x/Arthur-Shelby-Paul-Anderson-Peaky-Blinders-1263986.webp?r=1585830083062"
                         
                     />
                     <PreferencesCard 
                         name="Test Tested"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
+                        image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/14674251-low-res-peaky-blinders-iv-1568635012.jpg"
                     />
                     <PreferencesCard 
                         name="Test Abcdef"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
+                        image="https://i2-prod.dailypost.co.uk/incoming/article16870139.ece/ALTERNATES/s615b/3_Peaky-Blinders-V.jpg"
                     />
                     <PreferencesCard 
                         name="Test Ghijl"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
+                        image="https://i.inews.co.uk/content/uploads/2017/11/14466033-low_res-peaky-blinders-iv.jpg"
                     />
                     <PreferencesCard 
                         name="Test Defrhj"
-                        image="https://cdn.mos.cms.futurecdn.net/xYRAvSmUEJM7stp9V9T9VT.jpg"
+                        image="http://images6.fanpop.com/image/photos/43600000/Anya-Taylor-Joy-as-Gina-Gray-in-Peaky-Blinders-anya-taylor-joy-43670905-1334-2000.jpg"
                     />
                 </div>
             </div>
