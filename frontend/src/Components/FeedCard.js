4e48380f97b28aecff2a61f98e594ddcfc138767
Shreyash <shreyashkatare21@gmail.com>
2022-02-11 23:42:13 +0530

    added emoji picker

--- frontend/src/Components/FeedCard.js
+++ frontend/src/Components/FeedCard.js
@@ -1,5 +1,5 @@
 import { Avatar, IconButton } from '@mui/material';
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import "./FeedCard.css";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Carousel from './Carousel';
@@ -9,18 +9,37 @@ import AddCommentOutlinedIcon from '@mui/icons-material/AddCommentOutlined';
 import ShareIcon from '@mui/icons-material/Share';
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import BookmarkIcon from '@mui/icons-material/Bookmark';
+import Picker from 'emoji-picker-react';
+import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
 
 
 function FeedCard({name, lastname, username, image}) {
   const [like, setLike] = useState(false);
   const [bookmark, setBookmark] = useState(false);
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+  const [show, setShow] = useState(false);
+  const [comment, setComment] = useState("");
 
   const handleClick = () => {
     setLike(!like);
   };
+
   const handleClickBookmark = () => {
     setBookmark(!bookmark);
   };
+
+  const onEmojiClick = (event, emojiObject) => {
+    if(emojiObject !== null) {
+      var already = comment;
+      already += " " + emojiObject?.emoji;
+      setComment(already)
+    }
+  };
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+
   return (
     <div className="feedCard">
       <div className="feedCard__info">
@@ -112,6 +131,37 @@ function FeedCard({name, lastname, username, image}) {
         </div>
 
         </div>
+
+        <div className="feed__caption">
+          <span style={{margin:"12px 10px 0px 0px", fontWeight:"700"}}>test__user</span>
+          <span style={{lineHeight:"25px", fontSize:"16px"}}>Hey there this is a test. we have to work a lot and make this ui look more great. Looking forward for more photos and videos. #test #moretest</span>
+        </div>
+
+        <div className="feed__addComments">
+          <div>
+            <Avatar />
+          </div>
+          <div className="feed__enterComment">
+            <input placeholder="Add comment" value={comment} onChange={handleChange} />
+            <IconButton onClick={() => setShow(!show)}>
+              <EmojiEmotionsOutlinedIcon />
+            </IconButton>
+            {
+            show 
+            &&
+            <div style={{zIndex:"100", position:"absolute", top:"50px", right:"100px"}}>
+            <Picker
+              onEmojiClick={onEmojiClick}
+              disableAutoFocus={true}
+              groupNames={{ smileys_people: 'PEOPLE' }}
+              native
+            />
+          </div>
+            }
+          </div>
+          
+        </div>
+
     </div>
   );
 };
