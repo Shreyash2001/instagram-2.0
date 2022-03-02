99dfaf7745768b36e6d15eda17ef8758c7c97ad1
Shreyash <shreyashkatare21@gmail.com>
2022-03-02 09:57:30 +0530

    fixed the ui bug

--- frontend/src/Screens/HomeMiddle.js
+++ frontend/src/Screens/HomeMiddle.js
@@ -216,7 +216,7 @@ function HomeMiddle() {
         setCropData(data);
       };
 
-      const[url, setUrl] = useState(url);
+      const[url, setUrl] = useState(null);
       useEffect(() => {
         if(cropData){
             const data = new FormData()
