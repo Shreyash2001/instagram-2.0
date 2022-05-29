101d824e647c49fb4b91026d5cab600805b5c30f
Shreyash <shreyashkatare21@gmail.com>
2022-05-29 23:31:11 +0530

    started working on user details ui

--- frontend/src/Components/UserDetails.js
+++ frontend/src/Components/UserDetails.js
@@ -2,16 +2,49 @@ import React, { useEffect } from 'react';
 import "./UserDetails.css";
 import { getUserDetailsAction } from '../actions/userActions';
 import { useDispatch } from 'react-redux';
+import { Avatar } from '@mui/material';
 
 function UserDetails() {
     const username = window.location.pathname.split("/")[3];
     const dispatch = useDispatch();
+
+    const getData = () => {
+      dispatch(getUserDetailsAction(username));
+    }
+
     useEffect(() => {
-        dispatch(getUserDetailsAction(username));
+        getData();
     }, []);
 
   return (
     <div>
+        <div className="details__left">
+          <div className="details__profile">
+            <div className="details__top">
+              <Avatar style={{width:"120px", height:"120px"}} src="https://images2.minutemediacdn.com/image/fetch/w_736,h_485,c_fill,g_auto,f_auto/https%3A%2F%2Fwinteriscoming.net%2Ffiles%2F2018%2F07%2Fstranger-things-dragons-lair-850x560.jpg" />
+              <div>
+                <span>Testing tester</span>
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <h3>100</h3>
+                <span>Posts</span>
+              </div>
+              <div>
+                <h3>100</h3>
+                <span>Posts</span>
+              </div>
+              <div>
+                <h3>100</h3>
+                <span>Posts</span>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
         <div>
 
         </div>
