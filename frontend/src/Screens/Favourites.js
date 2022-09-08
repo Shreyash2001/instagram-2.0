5e51bfe11a107b921b598b6ad580eaf7bc2a3f80
Shreyash <shreyashkatare21@gmail.com>
2022-09-09 00:07:54 +0530

    started working on bookmark frontend

--- frontend/src/Screens/Favourites.js
+++ frontend/src/Screens/Favourites.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Favourites.css";
 
 function Favourites() {
+  const [temps, setTemps] = useState([1,2,3,4,5]);
   return (
     <div>
-        
+        {
+          temps?.map(() => (
+            <div>
+              <img src="https://static.dw.com/image/62850747_101.jpg" alt="" />
+            </div>
+          ))
+        }
     </div>
   )
-}
+};
 
 export default Favourites
