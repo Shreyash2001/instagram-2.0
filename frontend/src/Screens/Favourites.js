0b2b4ac985e6587fcde8651ccbb64eb853d8bcaf
Shreyash <shreyashkatare21@gmail.com>
2022-09-11 01:08:51 +0530

    worked on favourites ui page

--- frontend/src/Screens/Favourites.js
+++ frontend/src/Screens/Favourites.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import "./Favourites.css";
 import TopBar from "../Components/TopBar";
+import { Avatar } from '@mui/material';
 
 function Favourites() {
-  const [temps, setTemps] = useState([1,2,3,4,5]);
+  const [temps, setTemps] = useState([1,2,3,4,5,6]);
   return (
     <div className="favourites">
       <div className="topbar">
@@ -17,8 +18,23 @@ function Favourites() {
                   <img src="https://static.dw.com/image/62850747_101.jpg" alt="" />
                 </div>
                 <div>
-                  <div>
+                  <div className="user_information">
+                    <div>
+                      <Avatar />
+                    </div>
+                    <div>
+                      <div className="user_infonames">
+                        <div>
+                          <h3 style={{margin:"0px 0px 5px 0px", color:"#fff"}}>User Name</h3> 
+                        </div>
+                        <div>
+                          <span style={{color:"#fff", fontSize:"12px"}}>200k Followers</span>
+                        </div>
+                      </div>
 
+                      <div></div>
+                      
+                    </div>
                   </div>
                 </div>
               </div>
